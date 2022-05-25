621851a01f629dfad30c656eec278a4393454893
Luke Nemy <lukenemy7@gmail.com>
2022-05-24 14:12:33 -1000

    added test for correct url

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,12 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByTestId('learn-link');
   expect(linkElement).toBeInTheDocument();
 });
+
+test('url is correct', () => {
+  render(<App />);
+  const linkElement = screen.getByTestId('learn-link');
+  expect(linkElement.href).toContain('fasteazymoneyforme.com')
+})
